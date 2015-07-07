07529247375d2b610dd5f54404e7006311c80ef1
Фирулина Мария <rockerovka@mail.ru>
2015-07-07 13:15:18 +0300

    Юнит тестирование (начало)

---
+++ TestsLanguageTutorial/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LanguageTutorial;
+
+namespace TestsLanguageTutorial {
+    [TestClass]
+    public class TimerMetTest {
+        [TestMethod]
+        public void TestnumberSessionsLanguageEng() {
+            App.oCourseEnglish = null;
+            int res = 0;
+            int real = TimerMet.numberSessionsLanguageEng();
+            Assert.AreEqual(res, real, "Нет активного сеанса");
+        }
+    }
+}
